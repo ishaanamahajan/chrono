8f5492cec9326b983f866b4a34fcdc167d7194b6
Huzaifg <huzaifa.unjha@gmail.com>
2024-02-22 11:43:48 -0600

    AirSim GPS handler

---
+++ src/chrono_ros/handlers/sensor/ChROSAirSimGPSHandler.cpp
@@ -0,0 +1,106 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2023 projectchrono.org
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Huzaifa
+// =============================================================================
+//
+// ROS Handler for communicating gps information from AirSim
+//
+// =============================================================================
+
+#include "chrono_ros/handlers/sensor/ChROSAirSimGPSHandler.h"
+
+#include "chrono_ros/handlers/ChROSHandlerUtilities.h"
+#include "chrono_ros/handlers/sensor/ChROSSensorHandlerUtilities.h"
+
+#include "chrono_sensor/filters/ChFilterAccess.h"
+
+using namespace chrono::sensor;
+
+namespace chrono {
+namespace ros {
+
+ChROSAirSimGPSHandler::ChROSAirSimGPSHandler(std::shared_ptr<chrono::sensor::ChGPSSensor> gps,
+                                             const std::string& topic_name,
+                                             float hdop_init,
+                                             float vdop_init,
+                                             float hdop_final,
+                                             float vdop_final)
+    : ChROSAirSimGPSHandler(gps->GetUpdateRate(), gps, topic_name, hdop_init, vdop_init, hdop_final, vdop_final) {}
+
+ChROSAirSimGPSHandler::ChROSAirSimGPSHandler(double update_rate,
+                                             std::shared_ptr<chrono::sensor::ChGPSSensor> gps,
+                                             const std::string& topic_name,
+                                             float hdop_init,
+                                             float vdop_init,
+                                             float hdop_final,
+                                             float vdop_final)
+    : ChROSGPSHandler(update_rate, gps, topic_name),
+      m_eph_init(hdop_init),
+      m_epv_init(vdop_init),
+      m_eph(hdop_init),
+      m_epv(vdop_init),
+      m_eph_final(hdop_final),
+      m_epv_final(vdop_final),
+      m_eph_timeConstant(0.9f),
+      m_epv_timeConstant(0.9f),
+      m_lon_std(0.02f),
+      m_lat_std(0.02f),
+      m_alt_std(0.02f),
+      m_last_time(0.f) {}
+
+void ChROSAirSimGPSHandler::Tick(double time) {
+    auto gps_ptr = m_gps->GetMostRecentBuffer<UserGPSBufferPtr>();
+    if (!gps_ptr->Buffer) {
+        // TODO: Is this supposed to happen?
+        GetLog() << "GPS buffer is not ready. Not ticking. \n";
+        return;
+    }
+
+    GPSData gps_data = gps_ptr->Buffer[0];
+    m_gps_msg.header.stamp = ChROSHandlerUtilities::GetROSTimestamp(gps_data.Time);
+    m_gps_msg.latitude = gps_data.Latitude;
+    m_gps_msg.longitude = gps_data.Longitude;
+    m_gps_msg.altitude = gps_data.Altitude;
+
+    // Update the covariance matrix
+    // The ChGPSSensor does not currently support covariances, so we'll
+    // use the imu message to store a rolling average of the covariance
+    m_gps_msg.position_covariance = CalculateCovariance(gps_data);
+    m_gps_msg.position_covariance_type = sensor_msgs::msg::NavSatFix::COVARIANCE_TYPE_APPROXIMATED;
+
+    m_publisher->publish(m_gps_msg);
+    m_last_time = time;
+}
+
+void ChROSAirSimGPSHandler::FirstOrderFilter(double dt) {
+    float alpha = exp(-dt / m_eph_timeConstant);
+    // x(k+1) = Ad*x(k) + Bd*u(k)
+    m_eph = alpha * m_eph + (1 - alpha) * m_eph_final;
+    m_epv = alpha * m_epv + (1 - alpha) * m_epv_final;
+}
+
+std::array<double, 9> ChROSAirSimGPSHandler::CalculateCovariance(const GPSData& gps_data) {
+    // Get dt
+    double dt = gps_data.Time - m_last_time;
+    // Update the hdop and vdop
+    FirstOrderFilter(dt);
+    // Use filter to calculate the covariance -> For now m_epv is not used
+    std::array<double, 9> covariance;
+    covariance[0] = (m_eph * m_lon_std) * (m_eph * m_lon_std);
+    covariance[4] = (m_eph * m_lat_std) * (m_eph * m_lat_std);
+    covariance[8] = (2. * m_eph * m_alt_std) * (2. * m_eph * m_alt_dev);
+
+    return covariance;
+}
+
+}  // namespace ros
+}  // namespace chrono
---
+++ src/chrono_ros/handlers/sensor/ChROSAirSimGPSHandler.h
@@ -0,0 +1,104 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2023 projectchrono.org
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Huzaifa
+// =============================================================================
+//
+// ROS Handler for communicating gps information based on AirSim
+//
+// =============================================================================
+
+#ifndef CH_ROS_AirSim_GPS_HANDLER
+#define CH_ROS_AirSim_GPS_HANDLER
+
+#include "chrono_ros/ChROSGPSHandler.h"
+
+#include "chrono_ros/ChROSHandler.h"
+
+namespace chrono {
+namespace ros {
+
+/// @addtogroup ros_sensor_handlers
+/// @{
+
+/// This handler is responsible for interfacing a ChGPSSensor to ROS. Will publish sensor_msgs::msg::NavSatFix.
+class ChROSAirSimGPSHandler : public ChROSGPSHandler {
+  public:
+    /// Constructor. The update rate is set to gps->GetUpdateRate().
+    /// The update rate corresponds to the sensor's update rate.
+    ChROSAirSimGPSHandler(std::shared_ptr<chrono::sensor::ChGPSSensor> gps,
+                          const std::string& topic_name,
+                          float hdop_init,
+                          float vdop_init,
+                          float hdop_final,
+                          float vdop_final);
+
+    /// Full constructor. Takes a ChGPSSensor, update rate, and topic name.
+    ChROSAirSimGPSHandler(double update_rate,
+                          std::shared_ptr<chrono::sensor::ChGPSSensor> gps,
+                          const std::string& topic_name,
+                          float hdop_init,
+                          float vdop_init,
+                          float hdop_final,
+                          float vdop_final);
+
+  protected:
+    virtual void Tick(double time) override;
+
+  private:
+    /// Helper function to calculate the decay in hdop and vdop
+    /// Inspired from AirSim
+    /// https://github.com/microsoft/AirSim/blob/main/AirLib/include/common/FirstOrderFilter.hpp
+    void FirstOrderFilter(double dt);
+
+    /// Helper function to calculate the covariance of the accelerometer
+    /// ChGPSSensor currently doesn't support covariance
+    /// This is directly inspired from AirSim calculation of hdop's that are
+    /// used to calculate the covariance of the gps data using code from this driver
+
+    /// https://github.com/ros-drivers/nmea_navsat_driver/blob/06d71c7c5995fc8ef5579906d1d8097dbacf2c32/src/libnmea_navsat_driver/driver.py#L180C12-L198C60
+    std::array<double, 9> CalculateCovariance(const chrono::sensor::GPSData& gps_data, double time);
+
+  private:
+    std::shared_ptr<chrono::sensor::ChGPSSensor> m_gps;  ///< handle to the gps sensor
+
+    const std::string m_topic_name;                                         ///< name of the topic to publish to
+    sensor_msgs::msg::NavSatFix m_gps_msg;                                  ///< message to publish
+    rclcpp::Publisher<sensor_msgs::msg::NavSatFix>::SharedPtr m_publisher;  ///< the publisher for the gps message
+
+    // std::array<double, 3> m_running_average;
+
+    /// Parameters required for eph and epv calculation
+    float m_eph_init;   // Initial horizontal dilution of precision
+    float m_epv_init;   // Initial vertical dilution of precision
+    float m_eph_final;  // Final horizontal dilution of precision
+    float m_epv_final;  // Final vertical dilution of precision
+
+    float m_eph;  // Horizontal dilution of precision
+    float m_epv;  // Vertical dilution of precision
+
+    float
+        m_eph_timeConstant;  // Time constant for horizontal dilution of precision -> Used to improve eph exponentially
+    float m_epv_timeConstant;  // Time constant for vertical dilution of precision -> Used to improve epv exponentially
+
+    float m_lon_std;  // Constant based on type of gps sensor (see nmea_navsat_driver)
+    float m_lat_std;  // Constant based on type of gps sensor (see nmea_navsat_driver)
+    float m_alt_std;  // Constant based on type of gps sensor (see nmea_navsat_driver)
+
+    double m_last_time;  // Store last time to calculate dt needed in hdop/vdop calculation
+};
+
+/// @} ros_sensor_handlers
+
+}  // namespace ros
+}  // namespace chrono
+
+#endif
